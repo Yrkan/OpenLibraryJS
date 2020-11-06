b70039b39570fecbb0f29ba13d90577b203da68d
Thami Memel <memelthami@gmail.com>
2020-11-06 05:00:52 +0100

    update error messages with consts

--- server/routes/api/v1/admins.js
+++ server/routes/api/v1/admins.js
@@ -5,6 +5,16 @@ const bcrypt = require("bcryptjs");
 const { authAdmin } = require("../../../middlewears/auth");
 const router = Router();
 const Admin = require("../../../models/Admin");
+const {
+  USERNAME_ALREADY_IN_USE,
+  EMAIL_ALREADY_IN_USE,
+  EMAIL_ALREADY_VALIDATED,
+  INTERNAL_SERVER_ERROR,
+  INVALID_ID,
+  INVALID_TOKEN,
+  INVALID_CREDENTIALS,
+  UNAUTHORIZED_ACTION,
+} = require("../../../const/errors");
 
 // @Endpoint:       Get /api/v1/admins
 // @Description:    Get a list of all admins
@@ -12,20 +22,19 @@ const Admin = require("../../../models/Admin");
 router.get("/", authAdmin, async (req, res) => {
   try {
     const loggedAdmin = await Admin.findById(req.admin.id);
-    if (!loggedAdmin)
-      return res.status(401).json({ error: { msg: "Invalid Credentials" } });
+    if (!loggedAdmin) return res.status(401).json(INVALID_CREDENTIALS);
 
     // Only superadmins can get a list of all admins
     // IMPORTANT!!!! HAVING MULTIPLE SUPER ADMINS CAN CAUSE PROBLEMS !!!!
     if (!loggedAdmin.permissions.superAdmin) {
-      return res.status(401).json({ error: { msg: "Unauthorized Action" } });
+      return res.status(401).json(UNAUTHORIZED_ACTION);
     }
 
     const adminsList = await Admin.find();
     return res.send(adminsList);
   } catch (err) {
     console.error(err.message);
-    return res.status(500).json({ error: { msg: "Server Error" } });
+    return res.status(500).json(INTERNAL_SERVER_ERROR);
   }
 });
 
@@ -35,23 +44,22 @@ router.get("/", authAdmin, async (req, res) => {
 router.get("/:id", authAdmin, async (req, res) => {
   try {
     const loggedAdmin = await Admin.findById(req.admin.id);
-    if (!loggedAdmin)
-      res.status(401).json({ error: { msg: "Invalid Credentials" } });
+    if (!loggedAdmin) res.status(401).json(INVALID_CREDENTIALS);
 
     // Only superadmins or the admin with the request id can get the infos
     // IMPORTANT!!!! HAVING MULTIPLE SUPER ADMINS CAN CAUSE PROBLEMS !!!!
     if (loggedAdmin.permissions.superAdmin || req.admin.id === req.params.id) {
       const adminInfo = await Admin.findById(req.params.id);
       if (!adminInfo) {
-        return res.status(400).json({ error: { msg: "Invalid admin ID" } });
+        return res.status(400).json(INVALID_ID);
       }
 
       return res.json(adminInfo);
     }
-    return res.status(401).json({ error: { msg: "Unauthorized Action" } });
+    return res.status(401).json(UNAUTHORIZED_ACTION);
   } catch (err) {
     console.error(err.message);
-    return res.status(500).json({ error: { msg: "Server Error" } });
+    return res.status(500).json(INTERNAL_SERVER_ERROR);
   }
 });
 
@@ -77,27 +85,25 @@ router.post(
     try {
       const loggedAdmin = await Admin.findById(req.admin.id);
       if (!loggedAdmin) {
-        res.status(401).json({ error: { msg: "Invalid Credentials" } });
+        res.status(401).json(INVALID_CREDENTIALS);
       }
       // Only superadmins can create admins
       // IMPORTANT!!!! HAVING MULTIPLE SUPER ADMINS CAN CAUSE PROBLEMS !!!!
       if (!loggedAdmin.permissions.superAdmin) {
-        return res.status(401).json({ error: { msg: "Unauthorized Action" } });
+        return res.status(401).json(UNAUTHORIZED_ACTION);
       }
       const { username, password, email, permissions } = req.body;
 
       // Check if username is already in use
       const usernameInUse = await Admin.findOne({ username });
       if (usernameInUse) {
-        return res
-          .status(400)
-          .json({ error: { msg: "Username Already in use" } });
+        return res.status(400).json(USERNAME_ALREADY_IN_USE);
       }
 
       // Check if email is  already in use
       const emailInUse = await Admin.findOne({ email });
       if (emailInUse) {
-        return res.status(400).json({ error: { msg: "Email Already in use" } });
+        return res.status(400).json(EMAIL_ALREADY_IN_USE);
       }
       const salt = await bcrypt.genSalt(10);
       const hashed = await bcrypt.hash(password, salt);
@@ -112,7 +118,7 @@ router.post(
       return res.json(newAdmin);
     } catch (err) {
       console.error(err.message);
-      return res.status(500).json({ error: { msg: "Server Error" } });
+      return res.status(500).json(INTERNAL_SERVER_ERROR);
     }
   }
 );
@@ -136,7 +142,7 @@ router.put(
       // IMPORTANT!!!! HAVING MULTIPLE SUPER ADMINS CAN CAUSE PROBLEMS !!!!
       const loggedAdmin = await Admin.findById(req.admin.id);
       if (!loggedAdmin.permissions.superAdmin) {
-        return res.status(401).json({ error: { msg: "Unauthorized Access" } });
+        return res.status(401).json(UNAUTHORIZED_ACTION);
       }
 
       const { username, password, email, permissions } = req.body;
@@ -146,9 +152,7 @@ router.put(
       if (username) {
         // Make sure the updated username isn't used
         if (await Admin.findOne({ username })) {
-          return res
-            .status(400)
-            .json({ error: { msg: "Username already in use " } });
+          return res.status(400).json(USERNAME_ALREADY_IN_USE);
         }
         updates.username = username;
       }
@@ -157,9 +161,7 @@ router.put(
       if (email) {
         // Make sure the updated email isn't used
         if (await Admin.findOne({ email })) {
-          return res
-            .status(400)
-            .json({ error: { msg: "email already in use " } });
+          return res.status(400).json(EMAIL_ALREADY_IN_USE);
         }
         updates.email = email;
       }
@@ -206,14 +208,14 @@ router.put(
           return res.json(newUserInfo);
         }
         // no user with that id
-        return res.status(400).json({ error: { msg: "Invalid ID" } });
+        return res.status(400).json(INVALID_ID);
       } catch (err) {
         // TODO : BETTER ERROR HANDLING
-        return res.status(400).json({ error: { msg: "Invalid ID" } });
+        return res.status(400).json(INVALID_ID);
       }
     } catch (err) {
       console.error(err.message);
-      return res.status(500).json({ error: { msg: "Server Error" } });
+      return res.status(500).json(INTERNAL_SERVER_ERROR);
     }
   }
 );
@@ -226,25 +228,24 @@ router.delete("/:id", authAdmin, async (req, res) => {
     // Only superadmins can edit other admins
     // IMPORTANT!!!! HAVING MULTIPLE SUPER ADMINS CAN CAUSE PROBLEMS !!!!
     const loggedAdmin = await Admin.findById(req.admin.id);
-    if (!loggedAdmin)
-      res.status(401).json({ error: { msg: "Invalid Credentials" } });
+    if (!loggedAdmin) res.status(401).json(INVALID_CREDENTIALS);
 
     if (!loggedAdmin.permissions.superAdmin) {
-      return res.status(401).json({ error: { msg: "Unauthorized Access" } });
+      return res.status(401).json(UNAUTHORIZED_ACTION);
     }
     try {
       const deletedAdmin = await Admin.findByIdAndDelete(req.params.id);
       if (deletedAdmin) {
         return res.json(deletedAdmin);
       }
-      return res.status(400).json({ error: { msg: "Invalid Id" } });
+      return res.status(400).json(INVALID_ID);
     } catch (err) {
       console.error(err);
-      return res.status(400).json({ error: { msg: "Invalid Id" } });
+      return res.status(400).json(INVALID_ID);
     }
   } catch (err) {
     console.error(err.message);
-    return res.status(500).json({ error: { msg: "Server Error" } });
+    return res.status(500).json(INTERNAL_SERVER_ERROR);
   }
 });
 
